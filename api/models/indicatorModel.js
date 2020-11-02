c4ea90b371e51455baf32bf98b6052d88ad46b0b
Pedro Alan <pealan97@gmail.com>
2020-11-02 19:32:23 -0300

    Added indicator docs

--- api/models/indicatorModel.js
+++ api/models/indicatorModel.js
@@ -2,6 +2,33 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Indicator:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         number:
+ *           type: number
+ * 
+ *   IndicatorMongo:
+ *     allOf:
+ *       - $ref: '#/components/schemas/Indicator'
+ *       - type: object
+ *         properties:
+ *           _id:
+ *             type: string
+ *           __v:
+ *             type: number
+ * 
+ *   ListOfIndicators:
+ *     type: array
+ *     items:
+ *       $ref: '#/components/IndicatorMongo'
+ */
 var IndicatorSchema = new Schema(
   {
     name: {
--- api/routes/indicatorRoutes.js
+++ api/routes/indicatorRoutes.js
@@ -1,4 +1,101 @@
 'use strict';
+
+/**
+ * @swagger
+ * /indicator:
+ *   get:
+ *     tags: [Indicator]
+ *     summary: Gets the list of all indicators
+ *     responses:
+ *       200:
+ *         description: List of all indicators
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/ListOfIndicators'
+ * 
+ *   post:
+ *     tags: [Indicator]
+ *     summary: Registers a indicator
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#components/schemas/Indicator'
+ *     responses:
+ *       200:
+ *         description: The registered indicator
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/IndicatorMongo'
+ * 
+ *   delete:
+ *     tags: [Indicator]
+ *     summary: Deletes all indicators
+ *     responses:
+ *       200:
+ *         description: Delete was successful
+ * 
+ * /indicator/{indicatorId}:
+ *   get:
+ *     tags: [Indicator]
+ *     summary: Gets a indicator by id
+ *     parameters:
+ *       - in: path
+ *         name: indicatorId
+ *         schema:
+ *           type: string
+ *           required: true
+ *           description: The indicator id
+ *     responses:
+ *       200:
+ *         description: The indicator object matching the id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/IndicatorMongo'
+ *       404:
+ *         description: Indicator id not found
+ * 
+ *   put:
+ *     tags: [Indicator]
+ *     summary: Updates a indicator
+ *     parameters:
+ *       - in: path
+ *         name: indicatorId
+ *         schema:
+ *           type: string
+ *           required: true
+ *           description: The indicator id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#components/schemas/Indicator'
+ *     responses:
+ *       200:
+ *         description: Update was successful
+ * 
+ *   delete:
+ *     tags: [Indicator]
+ *     summary: Deletes a indicator by id
+ *     parameters:
+ *       - in: path
+ *         name: indicatorId
+ *         schema:
+ *           type: string
+ *           required: true
+ *           description: The indicator id
+ *     responses:
+ *       200:
+ *         description: Delete was successful
+ * 
+ * components:
+ *   $ref: '../models/indicatorModel.js'
+ */
 module.exports = function(app) {
   var indicator = require('../controllers/indicatorController');
 
