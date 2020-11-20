2ac985bfc16dcc561b80fefd493e26b08be92ab7
Joao Pedro Martins <joaop.congiomartins@gmail.com>
2020-11-20 01:24:07 -0300

    Add evaluation model and routes.

---
+++ api/models/evaluationModel.js
@@ -0,0 +1,35 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var AnswerGivenSchema = new Schema(
+  {
+    indicatorId: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    answer: {
+      type: String,
+      required: true
+    }
+  }
+);
+
+var EvaluationSchema = new Schema(
+  {
+    orgId: {
+      type: Schema.Types.ObjectId,
+      required: true
+    },
+    questions: {
+      type: [AnswerGivenSchema],
+      required: true
+    },
+    validated: Boolean
+  },
+  {
+    collection: 'Evaluation'
+  }
+);
+
+module.exports = mongoose.model('Evaluation', EvaluationSchema);
