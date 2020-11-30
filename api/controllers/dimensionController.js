8e895041ca5b5cf3fd83a64c6fcf2b62cb04fbd6
Pedro Alan <pealan97@gmail.com>
2020-11-30 00:36:58 -0300

    Fixed dimension endpoints

--- api/controllers/dimensionController.js
+++ api/controllers/dimensionController.js
@@ -6,65 +6,65 @@ var mongoose = require('mongoose'),
 exports.add = function(req, res) {
   const newDimension = new Dimension(req.body);
   newDimension.save(function(err, dimension) {
-    if (err) {
-      res.send(err);
+    if (dimension) {
+      res.status(200).json(dimension);
     } else {
-      res.json(dimension);
+      res.status(500).json({message: 'Unable to register dimension', error: err});
     }
   });
 };
 
 exports.update = function(req, res) {
+  const isValidId = mongoose.Types.ObjectId.isValid(req.params.dimensionId);
+  if (!isValidId) return res.status(404).json({message: 'Dimension id not found'});
+
   Dimension.findByIdAndUpdate(req.params.dimensionId, req.body, function(err, dimension) {
-    if (err) {
-      res.send(err);
-    } else if (!dimension) {
-      res.status(404).send("Dimension not found!");
+    if (dimension) {
+      res.status(200).send({...dimension._doc, ...req.body});
+    } else if (dimension == null && err == null) {
+      res.status(404).json({message: 'Dimension id not found'});
     } else {
-      res.send("Dimension matching the ID was modified!");
+      res.status(500).json({message: 'Unable to update dimension', error: err});
     }
   });
 };
 
 exports.get = function(req, res) {
+  const isValidId = mongoose.Types.ObjectId.isValid(req.params.dimensionId);
+  if (!isValidId) return res.status(404).json({message: 'Dimension id not found'});
+
   Dimension.findById(req.params.dimensionId, function(err, dimension) {
-    if (err) {
-      res.send(err);
+    if (dimension) {
+      res.status(200).json(dimension);
+    } else if (dimension == null && err == null) {
+      res.status(404).json({message: 'Dimension id not found'});
     } else {
-      res.json(dimension);
+      res.status(500).json({message: 'Unable to get dimension', error: err});
     }
   });
 };
 
 exports.listAll = function(req, res) {
   Dimension.find({}, function(err, dimension) {
-    if (err) {
-      res.send(err);
+    if (dimension) {
+      res.status(200).json(dimension);
     } else {
-      res.json(dimension);
+      res.status(500).json({message: 'Unable to list all dimensions', error: err});
     }
   });
 };
 
 exports.delete = function(req, res) {
-  Dimension.findByIdAndDelete(req.params.dimensionId, function(err, dimension) {
-    if (err) {
-      res.send(err);
-    } else if (!dimension) {
-      res.status(404).send("Dimension not found!");
-    } else {
-      res.send("Dimension matching the ID was deleted!");
-    }
-  });
-};
+  const isValidId = mongoose.Types.ObjectId.isValid(req.params.dimensionId);
+  if (!isValidId) return res.status(404).json({message: 'Dimension id not found'});
 
-// TODO: remove this before production
-exports.deleteAll = function(req, res) {
-  Dimension.deleteMany({}, function(err, dimension) {
-    if (err) {
-      res.send(err);
+  Dimension.findByIdAndDelete(req.params.dimensionId, function(err, dimension) {
+    if (dimension) {
+      res.status(200).json({message: 'Dimension successfully deleted'});
+    } else if (dimension == null && err == null) {
+      res.status(404).json({message: 'Dimension id not found'});
     } else {
-      res.send("All dimensions deleted!");
+      res.status(500).json({message: 'Unable to delete dimension', error: err});
     }
   });
 };
