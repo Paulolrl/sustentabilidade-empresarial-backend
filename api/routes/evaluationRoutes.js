5c4ca476d0d42ab5a65349fb218a66b92f3e2f7b
Pedro Alan <pealan97@gmail.com>
2020-12-03 01:58:04 -0300

    WIP: Initial swagger fix

--- api/routes/evaluationRoutes.js
+++ api/routes/evaluationRoutes.js
@@ -2,39 +2,33 @@
 
 /**
  * @swagger
- * /organization/{orgId}/evaluation/:
- *   parameters:
- *     - in: path
- *       name: orgId
- *       schema:
- *         type: string
- *         required: true
- *         description: The organization id
+ * /organization/mine/evaluation/:
  *   get:
  *     tags: [Evaluation]
- *     summary: Gets the list of registered evaluations
- *     description: "Gets a JSON list containing all evaluations entries
- *       inside the database. Your authorization token must have admin access."
+ *     summary: Gets all user's organization evaluations
+ *     description: "Gets all user's evaluations of their organization from
+ *       inside the database."
  *     responses:
  *       200:
  *         description: List of evaluation objects
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#components/ListOfEvaluations'
+ *               $ref: '#components/EvaluationMongo'
  *       401:
  *         description: No authorization token provided or authentication failed
  *       403:
  *         description: User does not have enough privileges
+ *       404:
+ *         description: This user does not have a organization
  *       500:
- *         description: Unable to list all evaluations
- *
+ *         description: Unable to get user's evaluation
+ * 
  *   post:
  *    tags: [Evaluation]
  *    summary: Registers an organization's evaluation
  *    description: "Receives an evaluation object and saves it into the database.
- *                  The evaluation will belong to an organization's specified by the id.
- *                  Your authorization token must have admin access."
+ *                  The evaluation will belong to this user's organization."
  *    requestBody:
  *      required: true
  *      content:
@@ -52,15 +46,113 @@
  *        description: JSON body with syntax error
  *      401:
  *        description: No authorization token provided or authentication failed
- *      403:
- *        description: User does not have enough privileges
  *      404:
- *        description: Organization id not found
+ *        description: This user does not have a organization
  *      500:
  *        description: Unable to register evaluation
+ * 
+ * /organization/{orgId}/evaluation/:
+ *   parameters:
+ *     - in: path
+ *       name: orgId
+ *       schema:
+ *         type: string
+ *         required: true
+ *       description: The organization id
+ *   get:
+ *     tags: [Evaluation]
+ *     summary: Gets the list of registered evaluations
+ *     description: "Gets a JSON list containing all evaluations entries
+ *       inside the database. Your authorization token must have admin access."
+ *     responses:
+ *       200:
+ *         description: List of evaluation objects
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/ListOfEvaluations'
+ *       401:
+ *         description: No authorization token provided or authentication failed
+ *       403:
+ *         description: User does not have enough privileges
+ *       404:
+ *         description: Organization id not found
+ *       500:
+ *         description: Unable to list all evaluations
  *
+ * /organization/mine/evaluation/{evaluationId}:
+ *   parameters:
+ *     - in: path
+ *       name: evaluationId
+ *       schema:
+ *         type: string
+ *         required: true
+ *       description: The evaluation id
+ *
+ *   get:
+ *     tags: [Evaluation]
+ *     summary: Gets an organization's evaluation by id
+ *     description: "Gets a single user's organization evaluation from inside the database.
+ *                   The evaluation must belong to this user's organization.
+ *                   The evaluation will be searched from the user organization which is
+ *                   related to their auth token."
+ *     responses:
+ *       200:
+ *         description: The evaluation object matching the id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/EvaluationMongo'
+ *       401:
+ *         description: No authorization token provided or authentication failed
+ *       404:
+ *         description: User do not have a organization or this evaluation does not exists
+ *       500:
+ *         description: Unable to get evaluation
+ *
+ *   put:
+ *     tags: [Evaluation]
+ *     summary: Updates an organization's evaluation
+ *     description: "Receives an evaluation object and updates it into the database.
+ *                   The evaluation must belong to this user's organization.
+ *                   The evaluation will be searched from the user organization which is
+ *                   related to their auth token."
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#components/schemas/Evaluation'
+ *     responses:
+ *       200:
+ *         description: Update was successful
+ *       400:
+ *         description: JSON body with syntax error
+ *       401:
+ *         description: No authorization token provided or authentication failed
+ *       404:
+ *         description: User do not have a organization or this evaluation does not exists
+ *       500:
+ *         description: Unable to update evaluation
+ *
+ *   delete:
+ *     tags: [Evaluation]
+ *     summary: Deletes an user's organization evaluation by its id
+ *     description: "Deletes this user's organization.
+ *                   The evaluation must belong to this user's organization.
+ *                   The evaluation will be searched from the user organization which is
+ *                   related to their auth token."
+ *     responses:
+ *       200:
+ *         description: Delete was successful
+ *       401:
+ *         description: No authorization token provided or authentication failed
+ *       404:
+ *         description: User do not have a organization or this evaluation does not exists
+ *       500:
+ *         description: Unable to delete evaluation
+ * 
  * /organization/{orgId}/evaluation/{evaluationId}:
- *
  *   parameters:
  *     - in: path
  *       name: orgId
@@ -78,28 +170,31 @@
  *
  *   get:
  *     tags: [Evaluation]
- *     summary: Gets the organization's evaluation by id
- *     description: "Gets a JSON list containing all evaluations entries
- *       inside the database. Your authorization token must have admin access."
+ *     summary: Gets an organization's evaluation by id
+ *     description: "Gets an evaluation by its id from inside the database.
+ *                   The evaluation must belong to the organization, which id must also be provided.
+ *                   Your authorization token must have admin access."
  *     responses:
  *       200:
- *         description: List of evaluation objects
+ *         description: The evaluation object matching the id
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#components/ListOfEvaluations'
+ *               $ref: '#components/EvaluationMongo'
  *       401:
  *         description: No authorization token provided or authentication failed
  *       403:
  *         description: User does not have enough privileges
+ *       404:
+ *         description: Organization or evaluation does not exist
  *       500:
- *         description: Unable to list all evaluations
+ *         description: Unable to get evaluation
  *
  *   put:
  *     tags: [Evaluation]
  *     summary: Updates an organization's evaluation
- *     description: "Receives an evaluation object and updates it into the database.
- *                   The evaluation must belong to the organization specified by id.
+ *     description: "Updates an evaluation by its id from inside the database.
+ *                   The evaluation must belong to the organization, which id must also be provided.
  *                   Your authorization token must have admin access."
  *     requestBody:
  *       required: true
@@ -109,7 +204,11 @@
  *             $ref: '#components/schemas/Evaluation'
  *     responses:
  *       200:
- *         description: Update was successful
+ *         description: The updated evaluation object matching the id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#components/EvaluationMongo'
  *       400:
  *         description: JSON body with syntax error
  *       401:
@@ -117,16 +216,16 @@
  *       403:
  *         description: User does not have enough privileges
  *       404:
- *         description: Organization or evaluation id not found
+ *         description: Organization or evaluation does not exist
  *       500:
  *         description: Unable to update evaluation
  *
  *   delete:
  *     tags: [Evaluation]
  *     summary: Deletes an organization's evaluation
- *     description: "Deletes an evaluation object by its id from the database.
- *                   The evaluation will be searched from the organization which id was
- *                   given. Your authorization token must have admin access."
+ *     description: "Deletes an evaluation by its id from inside the database.
+ *                   The evaluation must belong to the organization, which id must also be provided.
+ *                   Your authorization token must have admin access."
  *     responses:
  *       200:
  *         description: Delete was successful
@@ -135,12 +234,11 @@
  *       403:
  *         description: User does not have enough privileges
  *       404:
- *         description: Organization or evaluation id not found
+ *         description: Organization or evaluation does not exist
  *       500:
  *         description: Unable to delete evaluation
  *
  * /evaluation/{evaluationId}:
- *
  *   parameters:
  *     - in: path
  *       name: evaluationId
@@ -148,6 +246,7 @@
  *         type: string
  *         required: true
  *         description: The evaluation id
+ * 
  *   get:
  *     tags: [Evaluation]
  *     summary: Gets the evaluation by id
@@ -176,8 +275,8 @@ module.exports = function(app) {
   var auth = require('../auth/auth');
 
   app.route('/organization/mine/evaluation')
-    .post(auth.verifyToken, evaluation.add)
-    .get(auth.verifyToken, evaluation.listMine);
+    .get(auth.verifyToken, evaluation.listMine)
+    .post(auth.verifyToken, evaluation.add);
 
   app.route('/organization/:orgId/evaluation')
     .get(auth.verifyToken, auth.verifyAdmin, evaluation.listAllFromOrg);
