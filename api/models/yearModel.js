d0c708c1a37bce861bb9a06e6b4e6ec698527950
Pedro Alan <pealan97@gmail.com>
2020-12-09 00:02:24 -0300

    Added swagger

--- api/models/yearModel.js
+++ api/models/yearModel.js
@@ -2,6 +2,30 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Year:
+ *       type: object
+ *       properties:
+ *         year:
+ *           type: number
+ *   YearMongo:
+ *     allOf:
+ *       - $ref: '#/components/schemas/Year'
+ *       - type: object
+ *         properties:
+ *           _id:
+ *             type: string
+ *           __v:
+ *             type: number
+ *
+ *   ListOfYears:
+ *     type: array
+ *     items:
+ *       $ref: '#/components/YearMongo'
+ */
 var YearSchema = new Schema(
   {
     year: {
